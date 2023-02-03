cfb89bab12d57e79efa6947bcbb94db977dbd3b8
Takuya Oohashi <takuya.ohashi@fourier.jp>
2023-02-03 18:33:47 +0900

    Update to output an error log when a job fails

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Auth\PersonalAccessToken;
+use Illuminate\Queue\Events\JobFailed;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Laravel\Sanctum\Sanctum;
@@ -28,6 +29,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \Queue::failing(static function (JobFailed $event) {
+            \Log::error('ジョブが失敗しました。', [
+                'exception' => (string)$event->exception,
+                'connection' => $event->connectionName,
+                'job' => [
+                    'id' => $event->job->getJobId(),
+                    'name' => $event->job->getName(),
+                    'queue' => $event->job->getQueue(),
+                    'connection' => $event->job->getConnectionName(),
+                    'raw_body' => $event->job->getRawBody(),
+                ],
+            ]);
+        });
     }
 }
